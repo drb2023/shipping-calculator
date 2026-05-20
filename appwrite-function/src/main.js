dea738b745f3ca235e6ed8493587600a7c5ad69e
David Burkhart <dburkhart77@gmail.com>
2026-05-19 19:33:59 -0600

    Changed CORS for alternate site access

--- appwrite-function/src/main.js
+++ appwrite-function/src/main.js
@@ -1,5 +1,5 @@
 const CORS_HEADERS = {
-  "Access-Control-Allow-Origin": "https://db-shipping-calculator.netlify.app",
+  "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "POST, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type",
 };
